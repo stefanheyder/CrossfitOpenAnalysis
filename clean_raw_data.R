dc944cc381d011b8557abee4aaebe77e4307a7c8
Stefan Heyder <stefan.heyder@gmail.com>
2019-03-27 14:32:43 +0100

    First version of crossfit.Rmd

--- clean_raw_data.R
+++ clean_raw_data.R
@@ -3,7 +3,7 @@ library(tcltk)
 library(tidyverse)
 
 for(gender in c("male", "female")) {
-    file.information <- file.info(list.files("raw_data", pattern = gender, full.names = T))
+    file.information <- file.info(list.files("raw_data", pattern = paste("_",gender, sep = ""), full.names = T))
     latest.raw.file <- rownames(file.information)[which.max(file.information$mtime)]
 
     print(paste("Reading Raw Data File", latest.raw.file))
@@ -13,7 +13,7 @@ for(gender in c("male", "female")) {
     max.pages <- results[[1]]$pagination$totalPages
     timecaps <- c(15, 20, 10, 12, 20) * 60
 
-    print("Starting Data cleaning")
+    print(paste("Starting Data cleaning for", gender, "athletes"))
     pb <- txtProgressBar(min = 0, max = max.pages, style = 3)
     cleaned.results <- do.call(rbind,
         results %>% 
@@ -26,21 +26,28 @@ for(gender in c("male", "female")) {
                     scores <- results.of.page$leaderboardRows$scores 
 
                     do.call(rbind, scores %>%
-                        lapply(function(l) l[,3]) %>%
-                        lapply(as.numeric) %>%
-                        lapply(function(v) {
-                            tibble(remaining.seconds = v %% 10000, Repetitions = v %/% 10000 - 1000, WOD = c('19.1', '19.2', '19.3', '19.4', '19.5'))
+                        lapply(function(l) l[,c(3,7)]) %>%
+                        lapply(function(score.and.scaled) {
+                            score <- as.numeric(score.and.scaled$score)
+                            scaled <- score.and.scaled$scaled == "1"
+
+                            remaining.seconds <- score %% 10000
+                            Repetitions <- score %/% 10000 - 1000 * !scaled
+                            
+                            tibble(remaining.seconds = remaining.seconds, Repetitions = Repetitions, WOD = c('19.1', '19.2', '19.3', '19.4', '19.5'), Scaled = scaled)
     }) 
                         ) %>%
-                    mutate(Athelete.Name = rep(competitor.names, each = 5)) %>%
+                    mutate(FullName = rep(competitor.names, each = 5)) %>%
                     mutate(Athlete.Id = rep(competitor.ids, each = 5)) %>%
+                    mutate(Gender = gender) %>%
                     mutate(Time = rep(timecaps, length(scores)) -  remaining.seconds) %>%
                     select(-remaining.seconds)
                 } else {
                     list()
                 }
 
-}))
+})) %>% select(Athlete.Id, FullName, Gender, WOD, Scaled, Repetitions, Time)
+
     close(pb)
     print("Finished data cleaning")
     clean.file <- sub(".Rds", ".csv", basename(latest.raw.file))
