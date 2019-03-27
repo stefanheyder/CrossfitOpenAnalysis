204b909f17eb15bcefbba29f2e8d457eff1d6bd7
Stefan Heyder <stefan.heyder@gmail.com>
2019-03-27 12:21:20 +0100

    Handle failed requests in data

--- clean_raw_data.R
+++ clean_raw_data.R
@@ -3,7 +3,7 @@ library(tcltk)
 library(tidyverse)
 
 for(gender in c("male", "female")) {
-    file.information <- file.info(list.files("raw_data/", pattern = gender, full.names = T))
+    file.information <- file.info(list.files("raw_data", pattern = gender, full.names = T))
     latest.raw.file <- rownames(file.information)[which.max(file.information$mtime)]
 
     print(paste("Reading Raw Data File", latest.raw.file))
@@ -18,21 +18,28 @@ for(gender in c("male", "female")) {
     cleaned.results <- do.call(rbind,
         results %>% 
             lapply(function(results.of.page) {
-                setTxtProgressBar(pb, results.of.page$pagination$currentPage)
-                competitor.names <- results.of.page$leaderboardRows$entrant$competitorName
-                competitor.ids <- results.of.page$leaderboardRows$entrant$competitorId
-                scores <- results.of.page$leaderboardRows$scores 
-                do.call(rbind, scores %>%
-                    lapply(function(l) l[,3]) %>%
-                    lapply(as.numeric) %>%
-                    lapply(function(v) {
-                        tibble(remaining.seconds = v %% 10000, Repetitions = v %/% 10000 - 1000, WOD = c('19.1', '19.2', '19.3', '19.4', '19.5'))
-}) 
-                    ) %>%
-                mutate(Athelete.Name = rep(competitor.names, each = 5)) %>%
-                mutate(Athlete.Id = rep(competitor.ids, each = 5)) %>%
-                mutate(Time = rep(timecaps, length(scores)) -  remaining.seconds) %>%
-                select(-remaining.seconds)
+                if (is.null(results.of.page$statusCode)) {
+                    setTxtProgressBar(pb, results.of.page$pagination$currentPage)
+
+                    competitor.names <- results.of.page$leaderboardRows$entrant$competitorName
+                    competitor.ids <- results.of.page$leaderboardRows$entrant$competitorId
+                    scores <- results.of.page$leaderboardRows$scores 
+
+                    do.call(rbind, scores %>%
+                        lapply(function(l) l[,3]) %>%
+                        lapply(as.numeric) %>%
+                        lapply(function(v) {
+                            tibble(remaining.seconds = v %% 10000, Repetitions = v %/% 10000 - 1000, WOD = c('19.1', '19.2', '19.3', '19.4', '19.5'))
+    }) 
+                        ) %>%
+                    mutate(Athelete.Name = rep(competitor.names, each = 5)) %>%
+                    mutate(Athlete.Id = rep(competitor.ids, each = 5)) %>%
+                    mutate(Time = rep(timecaps, length(scores)) -  remaining.seconds) %>%
+                    select(-remaining.seconds)
+                } else {
+                    list()
+                }
+
 }))
     close(pb)
     print("Finished data cleaning")
